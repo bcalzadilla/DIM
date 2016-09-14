df3f627b598b6d98558abc467d52f646a2fd082c
bcalzadilla <bcalzadilla@outlook.com>
2016-09-14 13:22:04 -0500

    Agregada funcionalidad de ubicación GPS y mostrar datos en editText y botón rojo de prueba

--- app/src/main/java/com/dim/entuizer/dim/CameraActivity.java
+++ app/src/main/java/com/dim/entuizer/dim/CameraActivity.java
@@ -1,13 +1,27 @@
 package com.dim.entuizer.dim;
 
+import android.content.Context;
+import android.hardware.Camera;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class CameraActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
-    }
+
 }
